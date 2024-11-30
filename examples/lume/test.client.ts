06b9ed337ece24f31829e35263e7ea0b777a55ef
JLarky <jlarky@gmail.com>
2024-11-30 13:36:35 -0700

    add e2e tests

--- examples/lume/test.client.ts
+++ examples/lume/test.client.ts
@@ -1,7 +1,18 @@
 import { liftHtml } from "../../packages/lift-html/mod.ts";
 
-liftHtml("lift-counter", {
+const LiftCounter = liftHtml("lift-counter", {
+  observedAttributes: ["count"] as const,
   init() {
-    console.log("init", this);
+    const render = () => {
+      const count = this.getAttribute("count") || "-";
+      const div = document.createElement("div");
+      div.className = "loaded";
+      div.textContent = count;
+      this.innerHTML = div.outerHTML;
+    };
+    this.acb = render;
+    render();
   },
 });
+
+console.log("static", LiftCounter.observedAttributes);
