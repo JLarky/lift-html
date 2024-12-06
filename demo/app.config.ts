99199584b30dd07fb7e14bc5336e4db46df1b3e0
JLarky <jlarky@gmail.com>
2024-12-06 00:51:48 -0700

    new demo start

--- demo/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "@solidjs/start/config";
-
-export default defineConfig({
-    // to make the app server-side only
-    experimental: {
-        islands: true,
-    },
-});
