df8cb55049575bf307d1b01bc9ef0c1387622d7f
JLarky <jlarky@gmail.com>
2024-12-08 00:45:28 -0700

    npm publishing

---
+++ scripts/publish_npm.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write --allow-net --allow-env --allow-run
+import $ from "jsr:@david/dax";
+
+$.cd($.path(new URL(".", import.meta.url)));
+
+const dryRun = Deno.args.includes("--dry-run");
+
+// core
+
+await $`./build_core.ts`;
+
+if (dryRun) {
+  await $`cd ../packages/core/npm/; npm publish --dry-run`;
+} else {
+  await $`cd ../packages/core/npm/; npm publish`;
+}
