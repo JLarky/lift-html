dc36af700491bde72431540d84c5966603ebc101
JLarky <jlarky@gmail.com>
2025-02-24 02:33:17 -0700

    cursor: add e2e test for incentive

---
+++ examples/lume/incentive.client.ts
@@ -0,0 +1,23 @@
+import { targetRefs } from "@lift-html/incentive";
+import { liftHtml } from "@lift-html/core";
+
+liftHtml("lift-counter", {
+  observedAttributes: ["count"] as const,
+  init() {
+    const refs = targetRefs(this, {
+      output: [HTMLDivElement, true],
+    });
+
+    const div = document.createElement("div");
+    div.className = "loaded";
+    div.textContent = this.getAttribute("count") || "1";
+    div.dataset.target = `${this.tagName.toLowerCase()}:output`;
+    this.appendChild(div);
+
+    this.acb = (name) => {
+      if (name === "count") {
+        refs.output.textContent = this.getAttribute("count") || "1";
+      }
+    };
+  },
+});
