5f4d37db279625dfbb4bdc84c74cd37e3b00196c
JLarky <jlarky@gmail.com>
2025-07-22 20:34:09 -0600

    counter examples

---
+++ docs/src/components/examples/vanilla-counter/vanilla-counter.ts
@@ -0,0 +1,22 @@
+// define a custom element
+export class MyButton extends HTMLElement {
+  connectedCallback() {
+    const button = this.querySelector("button");
+    if (!button) throw new Error("<my-button> must contain a <button>");
+    button.disabled = false;
+    let count = 0;
+    button.onclick = () => {
+      count++;
+      button.textContent = `Clicks: ${count}`;
+    };
+  }
+}
+
+if (typeof customElements !== "undefined") {
+  if (customElements.get("my-button")) {
+    console.warn(
+      "Can't redefine custom element <my-button>. Page reload required."
+    );
+  }
+  customElements.define("my-button", MyButton);
+}
