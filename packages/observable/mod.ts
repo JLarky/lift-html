5f3d637be7973bd782b73e4f1410495525fa854d
JLarky <jlarky@gmail.com>
2025-08-10 22:42:05 -0600

    observable WIP

---
+++ packages/observable/mod.ts
@@ -0,0 +1,90 @@
+// PUBLIC DOMAIN: https://github.com/JLarky/lift-html/blob/main/packages/observable/mod.ts
+
+import {
+  type Attributes,
+  type LiftBaseClass,
+  type LiftBaseConstructor,
+  liftHtml,
+  type LiftOptions,
+} from "@lift-html/core";
+export type {
+  Attributes,
+  Htmlify,
+  KnownElements,
+  LiftBaseClass,
+  LiftBaseConstructor,
+  LiftOptions,
+  Reactify,
+  Solidify,
+} from "@lift-html/core";
+
+/**
+ * Creates a custom element. The `init` function is called when the element is
+ * connected to the DOM, and you can safely use alien-signals reactive primitives
+ * like `effect` and `signal` inside it.
+ *
+ * @example
+ *```ts
+ * // rendered with <hello-el name="world"></hello-el>
+ * import { liftAlien, useAttributes } from "@lift-html/alien";
+ * import { effect } from "alien-signals";
+ *
+ * liftAlien("hello-el", {
+ *   observedAttributes: ["name"],
+ *   init() {
+ *     const props = useAttributes(this);
+ *     effect(() => {
+ *       this.innerText = "Hello, " + props.name;
+ *     });
+ *   },
+ * });
+```
+ */
+// export function liftObservable<
+//   TAttributes extends Attributes,
+//   Options extends LiftOptions<TAttributes>,
+// >(
+//   tagName: string,
+//   opts: Partial<LiftOptions<TAttributes>>,
+// ): LiftBaseConstructor<TAttributes, Options> {
+//   return liftHtml(tagName, {
+//     ...opts,
+//     init(dispose) {
+//       dispose(effectScope(() => {
+//         opts.init?.call(this, dispose);
+//       }));
+//     },
+//   });
+// }
+
+/**
+ * Makes attributes reactive. Returns an object where each key is a signal
+ * based on the `options.observedAttributes` of the component.
+ */
+export function useAttributes<TAttributes extends Attributes>(
+  instance: LiftBaseClass<TAttributes, LiftOptions<TAttributes>>,
+): Record<NonNullable<TAttributes>[number], () => string | null> {
+  const attributes = instance.options.observedAttributes as TAttributes;
+  const props = {} as Record<
+    NonNullable<TAttributes>[number],
+    () => string | null
+  >;
+  type SignalType = ReturnType<typeof signal<string | null>>;
+  const signals = new Map<string, SignalType>();
+
+  if (attributes) {
+    for (const key of attributes) {
+      const sig = signal<string | null>(instance.getAttribute(key));
+      signals.set(key, sig);
+      // Cast key to the correct type since we know it's from attributes
+      props[key as NonNullable<TAttributes>[number]] = () => sig();
+    }
+    instance.acb = (attrName: string, newValue: string | null) => {
+      const sig = signals.get(attrName);
+      if (sig) {
+        sig(newValue);
+      }
+    };
+  }
+  return props;
+}
---
+++ packages/observable/mod_test.ts
@@ -0,0 +1,36 @@
+import { assertEquals } from "jsr:@std/assert";
+// import { assertSpyCallArgs, assertSpyCalls, spy } from "jsr:@std/testing/mock";
+
+import { liftAlien, useAttributes } from "./mod.ts";
+
+// Deno.test("this crashes in server build unfortunatelly", () => {
+//   const mockFn = spy((_element: HTMLElement) => {});
+//   const TestElement = liftAlien("test-element", {
+//     observedAttributes: ["test"],
+//     init() {
+//       mockFn(this);
+//     },
+//   });
+//   const element = new TestElement();
+//   Object.defineProperty(element, "isConnected", { value: true });
+//   element.connectedCallback();
+//   //   element.attributeChangedCallback("test", "old", "new");
+// });
+
+Deno.test("useAttributes creates props", () => {
+  const TestElement = liftAlien("test-element", {
+    observedAttributes: ["count"],
+  });
+  const el = new TestElement();
+  el.getAttribute = () => "1";
+  assertEquals(typeof el.acb, "undefined");
+  const props = useAttributes(el);
+  assertEquals(typeof el.acb, "function");
+  assertEquals(props.count(), "1");
+  // other props do nothing
+  el.acb!("not-count", "10");
+  assertEquals(props.count(), "1");
+  // changing count updates props
+  el.acb!("count", "10");
+  assertEquals(props.count(), "10");
+});
